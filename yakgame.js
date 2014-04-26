4f5e88dad653a510f74d331871c72143591abd43
Pēteris Paikens <PeterisP@gmail.com>
2014-04-26 22:57:17 +0300

    first job - movement

--- yakgame.js
+++ yakgame.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var stage;
+var fons;
 var lblFPS;
 var log_area;
 
 var dwarves = [];
 var dwarfpics;
-
 
 var floorstart = 240;
 var floorheight = 128;
@@ -15,7 +17,8 @@ var floorpics;
 var floors = [];
 
 var move_speed = 0.05; // Chance to move at each 60fps tick.
-
+var dwarfstep = 16; // step size in px
+var dwarfwidth = 64 // dwarf sprite width
 
 function init() {
   log_area = document.getElementById("log");
@@ -36,7 +39,7 @@ function init() {
 
   dwarfpics = new createjs.SpriteSheet({
     images: ["ruukji.png"],
-    frames: {width:64, height:64},
+    frames: {width:dwarfwidth, height:64},
     animations: {
       hauler:0,
       warrior:1,
@@ -50,9 +53,6 @@ function init() {
 function setup() {
   stage.removeAllChildren();
   stage.addChild(fons);
-  lblFPS.x = 10;
-  lblFPS.y = 10;
-  stage.addChild(lblFPS);
 
   createjs.Ticker.init();
   createjs.Ticker.timingMode = createjs.Ticker.RAF;
@@ -72,6 +72,11 @@ function setup() {
   dwarves.push(new Dwarf("hauler", 0,100,"Urists 1"));
   dwarves.push(new Dwarf("warrior", 1,200,"Urists 2"));
   dwarves.push(new Dwarf("craftsman", 2,300,"Urists 3"));
+  dwarves[0].add_job(new Job_WalkTo(3,30));
+
+  lblFPS.x = 10;
+  lblFPS.y = 10;
+  stage.addChild(lblFPS);
 
   log("setup done");
 }
@@ -101,10 +106,10 @@ function Dwarf(pic, floor, x, name) {
   this.x = x;
   this.floor = floor;
   this.name = name;
-  this.job = new Job_RandomWalk();
+  this.jobs = [];
   this.update();
   stage.addChild(this.sprite);
-  dwarf = this;
+  var dwarf = this;
   this.sprite.addEventListener("click", function(event) {
     console.log(dwarf);
     log(dwarf.name + " -> " + dwarf.job.description);
@@ -113,38 +118,88 @@ function Dwarf(pic, floor, x, name) {
 Dwarf.prototype.sprite = null;
 Dwarf.prototype.name = "Urist McDwarf";
 Dwarf.prototype.update = function() {
-  this.job.doit(this);
+  var job;
+  if (this.jobs.length>0) {
+    job = this.jobs[0];
+  } else {
+    job = Job_RandomWalk;
+  }
+  job.doit(this);
   this.sprite.x = this.x;
   this.sprite.y = floorstart + (this.floor)*floorheight + floormargin;
 }
 
-function Job() {}
-Job.prototype.doit = function(dwarf) {
-  log("Abstract job called ...");
-  log(dwarf);
+Dwarf.prototype.add_job = function(job) {
+  this.jobs.push(job);
 }
-Job_RandomWalk.prototype.name = "Abstract job";
-Job_RandomWalk.prototype.description = "This job shouldn't ever be assigned";
-
-function Job_RandomWalk() {}
-Job_RandomWalk.prototype.doit = function(dwarf) {
-  if (Math.random() < move_speed) {
-    if (Math.random()<0.5){
-      dwarf.x -= 16;
-    } else {
-      dwarf.x += 16;
-    }
-    if (dwarf.x < floormargin) {dwarf.x = floormargin;}
-    if (dwarf.x > floorwidth-dwarf.sprite.width-floormargin) {dwarf.x = floorwidth-dwarf.sprite.width-floormargin;}
+Dwarf.prototype.job_done = function(job) {
+  log(this.name + ": " + job.name + " done!");
+  var job_index = this.jobs.indexOf(job);
+  if (job_index > -1) {
+    this.jobs.splice(job_index, 1);
+  } else {
+    log(this.name() + ": finished job that he didn't have...")
   }
 }
-Job_RandomWalk.prototype.name = "Random walk";
-Job_RandomWalk.prototype.description = "I'm randomly walking around and thinking about booze";
+
+// Job structure:
+// name - short name used in status reports
+// description - long name used in showing what a dwarf is doing
+// doit(dwarf) - do a tick of that job
+
+var Job_RandomWalk = {
+  name : "Random walk",
+  description : "I'm randomly walking around and thinking about booze",
+  doit : function(dwarf) {
+    if (Math.random() < move_speed) {
+      if (Math.random()<0.5){
+        dwarf.x -= dwarfstep;
+      } else {
+        dwarf.x += dwarfstep;
+      }
+      if (dwarf.x < floormargin) {dwarf.x = floormargin;}
+      if (dwarf.x > floorwidth-dwarfwidth-floormargin) {dwarf.x = floorwidth-dwarfwidth-floormargin;}
+    }
+  }
+}
 
 function Job_WalkTo(floor, x) {
   this.targetfloor = floor;
   this.targetx = x;
+  this.name = "Go to floor "+floor;
+  this.description = "Walking to floor "+floor+" spot "+x;
 }
 Job_WalkTo.prototype.doit = function(dwarf) {
-
+  if (Math.random() < move_speed) {
+    if (dwarf.floor == this.targetfloor) {
+      var distance = this.targetx-dwarf.x;
+      if (Math.abs(distance) <= dwarfstep) {
+        // We've arrived
+        dwarf.x = this.targetx;
+        dwarf.job_done(this);
+      } else {
+        // make a step towards the target
+        var step = dwarfstep * distance/Math.abs(distance);
+        dwarf.x += step;
+      }
+    } else { // Different floor
+      // how far until middle of the room?
+      var stairs = floorwidth/2-dwarfwidth/2;
+      var distance = stairs - dwarf.x;
+      if (distance == 0) {
+        // change floors
+        if (dwarf.floor > this.targetfloor)
+          {dwarf.floor--;}
+        else
+          {dwarf.floor++;}
+      } else if (Math.abs(distance) <= dwarfstep) {
+        // Move to the stairs
+        dwarf.x = stairs;
+      } else {
+        // make a step towards the stairs;
+        var step = dwarfstep * distance/Math.abs(distance);
+        dwarf.x += step;
+      }
+    }
+  }
 }
